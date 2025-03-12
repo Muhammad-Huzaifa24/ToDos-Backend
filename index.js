8fb14a4125d8032c8d5ef8e7bdc87166bb0504a5
Muhammad <huzaifakhawar100@gmail.com>
2025-03-12 12:40:46 +0500

    update cors policy

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors());
+// app.use(cors());
+
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	})
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
